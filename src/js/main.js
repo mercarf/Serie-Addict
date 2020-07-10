d61e139fd862cb3e945a15a4c06228d6e264c01c
Mercedes <mcarballalfr@gmail.com>
2020-07-10 12:15:38 +0200

    update series & fav

--- src/js/main.js
+++ src/js/main.js
@@ -5,53 +5,90 @@ let searchSeries = [];
 let favSeries = [];
 
 //Constantes
-const inputSearch = document.querySelector('.js-input-search');
-const buttonSearch = document.querySelector('.js-button-search');
+const form = document.querySelector('.form-search');
 
 // api
 
-const getDataFromApi = () => {
-  fetch(`http://api.tvmaze.com/search/shows?q=girls`)
+const getDataFromApi = (ev) => {
+  const inputSearchValue = document.querySelector('.js-input-search').value;
+  ev.preventDefault();
+  console.log('Han pulsado el botón');
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputSearchValue}`)
     .then((response) => response.json())
     .then((data) => {
       searchSeries = data;
+      paintSeriesSearch();
     });
 };
 
 //Paint
 const paintSeriesSearch = () => {
+  const listSeries = document.querySelector('.js-list-series');
+  const defaultImage =
+    'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
   let codeHTML = '';
   for (let serieData of searchSeries) {
     const serie = serieData.show;
-    if (serie.image === null) {
-      serieImage.src = defaultImage;
-    }
-    codeHTML += `<li class="serie">`;
+
+    codeHTML += `<li class="serie js-serie" id="${serie.id}">`;
     codeHTML += `<h2 id="${serie.id}" class="serie__title">${serie.name}</h2>`;
-    codeHTML += `<img src="${serie.image.medium}" id="${serie.id}" class="serie__img" alt="Serie ${serie.name}" />`;
-    codeHTML += `<p class="serie__summary">${serie.summary}</p>`;
+    if (serie.image !== null) {
+      codeHTML += `<img src="${serie.image.medium}" id="${serie.id}" class="js-serie__img" alt="Serie ${serie.name}" />`;
+    } else {
+      codeHTML += `<img src="${defaultImage}" id="${serie.id}" class="serie__img" alt="Serie ${serie.name}" />`;
+    }
+    codeHTML += `<p class="serie__summary" id="${serie.id}">${serie.summary}</p>`;
     codeHTML += `</li>`;
+    listSeries.innerHTML = codeHTML;
   }
-  const defaultImage =
-    'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-  const serieImage = document.querySelector('.serie__img');
-  const listSeries = document.querySelector('.js-list-series');
-  listSeries.innerHTML = codeHTML;
+  listenProductsClicks();
 };
 
+const paintSeriesFav = () => {
+  const listFav = document.querySelector('.js-list-favorites');
+  const defaultImage =
+    'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+  let codeHTML = '';
+  for (let serieData of favSeries) {
+    const serie = serieData.show;
+
+    codeHTML += `<li class="serie js-serie" id="${serie.id}">`;
+    codeHTML += `<h2 id="${serie.id}" class="serie__title">${serie.name}</h2>`;
+    if (serie.image !== null) {
+      codeHTML += `<img src="${serie.image.medium}" id="${serie.id}" class="js-serie__img" alt="Serie ${serie.name}" />`;
+    } else {
+      codeHTML += `<img src="${defaultImage}" id="${serie.id}" class="serie__img" alt="Serie ${serie.name}" />`;
+    }
+    codeHTML += `</li>`;
+    listFav.innerHTML = codeHTML;
+  }
+};
+
+//-------------------FAVORITES-----------------------
+
+const handleSerieClick = (ev) => {
+  // obtenemos el id del producto clickado
+  const clickedId = parseInt(ev.currentTarget.id);
+  // buscamos con find
+  const serie = searchSeries.find(
+    (productItem) => productItem.show.id === clickedId
+  );
+  console.log(serie);
+  favSeries.push(serie);
+  paintSeriesFav();
+};
 //-----------------LOCAL STORAGE------------------------------
 
 // listener
-function handleButtonClick(ev) {
-  ev.preventDefault();
-  console.log('Han pulsado el botón');
-  console.log(paintSeriesSearch());
-  paintSeriesSearch();
-  console.log(searchSeries);
-}
 
-buttonSearch.addEventListener('click', handleButtonClick);
+const listenProductsClicks = () => {
+  const seriesAll = document.querySelectorAll('.js-serie');
+  for (let index = 0; index < seriesAll.length; index++) {
+    const productsBtn = seriesAll[index];
+    productsBtn.addEventListener('click', handleSerieClick);
+  }
+};
+
+form.addEventListener('submit', getDataFromApi);
 
 // start app
-
-getDataFromApi();
