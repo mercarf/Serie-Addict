8ca5f2942db44a6e1b1d151b567f332086dd17ad
Mercedes <mcarballalfr@gmail.com>
2020-07-10 15:53:16 +0200

    update favorites

--- src/js/main.js
+++ src/js/main.js
@@ -6,22 +6,31 @@ let favSeries = [];
 
 //Constantes
 const form = document.querySelector('.form-search');
+const inputSearch = document.querySelector('.js-input-search');
 
 // api
 
 const getDataFromApi = (ev) => {
-  const inputSearchValue = document.querySelector('.js-input-search').value;
+  const inputSearchValue = inputSearch.value;
   ev.preventDefault();
-  console.log('Han pulsado el botón');
   fetch(`http://api.tvmaze.com/search/shows?q=${inputSearchValue}`)
     .then((response) => response.json())
     .then((data) => {
       searchSeries = data;
-      paintSeriesSearch();
+      if (inputSearchValue !== '') {
+        paintSeriesSearch();
+      } else {
+        paintError();
+      }
     });
 };
 
 //Paint
+function paintError() {
+  const errorMessage = document.querySelector('.js-error-message');
+  errorMessage.innerHTML = 'No has introducido nintun dato';
+}
+
 const paintSeriesSearch = () => {
   const listSeries = document.querySelector('.js-list-series');
   const defaultImage =
@@ -42,6 +51,7 @@ const paintSeriesSearch = () => {
     listSeries.innerHTML = codeHTML;
   }
   listenProductsClicks();
+  inputSearch.value = '';
 };
 
 const paintSeriesFav = () => {
@@ -52,43 +62,104 @@ const paintSeriesFav = () => {
   for (let serieData of favSeries) {
     const serie = serieData.show;
 
-    codeHTML += `<li class="serie js-serie" id="${serie.id}">`;
-    codeHTML += `<h2 id="${serie.id}" class="serie__title">${serie.name}</h2>`;
+    codeHTML += `<li class="serie js-fav" id="${serie.id}">`;
+    codeHTML += `<h2 id="${serie.id}" class="fav__title">${serie.name}</h2>`;
     if (serie.image !== null) {
-      codeHTML += `<img src="${serie.image.medium}" id="${serie.id}" class="js-serie__img" alt="Serie ${serie.name}" />`;
+      codeHTML += `<img src="${serie.image.medium}" id="${serie.id}" class="js-fav__img" alt="Serie ${serie.name}" />`;
     } else {
-      codeHTML += `<img src="${defaultImage}" id="${serie.id}" class="serie__img" alt="Serie ${serie.name}" />`;
+      codeHTML += `<img src="${defaultImage}" id="${serie.id}" class="fav__img" alt="Serie ${serie.name}" />`;
     }
     codeHTML += `</li>`;
     listFav.innerHTML = codeHTML;
   }
+  listenFavsClicks();
 };
 
 //-------------------FAVORITES-----------------------
 
+//cuando hacemo click en el listado de series
 const handleSerieClick = (ev) => {
   // obtenemos el id del producto clickado
   const clickedId = parseInt(ev.currentTarget.id);
   // buscamos con find
-  const serie = searchSeries.find(
+  const serieFind = searchSeries.find(
     (productItem) => productItem.show.id === clickedId
   );
-  console.log(serie);
-  favSeries.push(serie);
+
+  const seriesclick = ev.currentTarget;
+  seriesclick.classList.toggle('favorite');
+
+  const seriefav = favSeries.find(
+    (productItem) => productItem.show.id === clickedId
+  );
+  if (seriefav === undefined) {
+    favSeries.push(serieFind);
+  }
+
+  console.log(serieFind);
+  paintSeriesFav();
+  updateLocalStorage();
+};
+
+//Cuando hacemos click en los favoritos
+const handleFavsClick = (ev) => {
+  const clickedId = parseInt(ev.currentTarget.id);
+  console.log(clickedId);
+  // podriamos utilizar find index para buscar la posición del elemento a borrar
+  const index = favSeries.findIndex(
+    (productItem) => productItem.show.id === clickedId
+  );
+  console.log(index);
+
+  favSeries.splice(index, 1);
+  updateLocalStorage();
   paintSeriesFav();
 };
+
 //-----------------LOCAL STORAGE------------------------------
+
+const updateLocalStorage = () => {
+  localStorage.setItem('Favorites', JSON.stringify(favSeries));
+};
+
+const getFromLocalStorage = () => {
+  const saveFavorites = JSON.parse(localStorage.getItem('Favorites'));
+  if (saveFavorites !== null) {
+    favSeries = saveFavorites;
+    paintSeriesFav(); //Cuando tengamos favoritos guardados en LS aparecerán al refrescar en la lista de favoritos
+  }
+};
+// Reset
+
+/* const btnReset = document.querySelector('.js-reset-btn');
+
+const resetCart = () => {
+  favSeries = [];
+  updateLocalStorage();
+  getFromLocalStorage();
+};
+
+btnReset.addEventListener('click', resetCart); */
 
 // listener
 
 const listenProductsClicks = () => {
   const seriesAll = document.querySelectorAll('.js-serie');
   for (let index = 0; index < seriesAll.length; index++) {
-    const productsBtn = seriesAll[index];
-    productsBtn.addEventListener('click', handleSerieClick);
+    const serieFind = seriesAll[index];
+    serieFind.addEventListener('click', handleSerieClick);
   }
 };
 
-form.addEventListener('submit', getDataFromApi);
+const listenFavsClicks = () => {
+  const favsAll = document.querySelectorAll('.js-fav');
+  for (let index = 0; index < favsAll.length; index++) {
+    const serieFav = favsAll[index];
+    serieFav.addEventListener('click', handleFavsClick);
+  }
+};
+
+form.addEventListener('submit', getDataFromApi); //Se ejecuta al hacer un submit en el formulario (no solo con click en el boton sino al pulsar enter tambien)
 
 // start app
+getFromLocalStorage(); //se ejecuta al iniciar la página
